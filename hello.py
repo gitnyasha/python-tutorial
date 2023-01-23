957594153dae96b1b9d4307b872bc3647f314410
gitnyasha <marshall.chikari@gmail.com>
2023-01-23 14:03:55 +0200

    add date

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    dayname = day[datetime.now().weekday()]
+    return f'<h1>Hello, World! Happy {dayname}</h1>'
